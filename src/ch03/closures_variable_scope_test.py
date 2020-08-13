7a0931d40ae99270a8a5bf621f9281204dd306d7
jashburn <jashburn8020@gmail.com>
2020-08-13 02:00:52 +0100

    Simplify examples for item 21.

--- src/ch03/closures_variable_scope_test.py
+++ src/ch03/closures_variable_scope_test.py
@@ -3,29 +3,25 @@
 Know How Closures Interact with Variable Scope.
 """
 
-from typing import List, Set, Tuple
+from typing import Set, Tuple
 
 
 def test_sort_not_found() -> None:
     """Variable does not exist in the current scope."""
-
-    def sort_priority_fail(numbers: List[int], group: Set[int]) -> bool:
-        found = False
-
-        def sort_key(elem: int) -> Tuple[int, int]:
-            if elem in group:
-                # `found` does not exist in the current scope; the assignment is treated
-                # as a variable definition, scoped within this function.
-                found = True
-                return (0, elem)
-            return (1, elem)
-
-        numbers.sort(key=sort_key)
-        return found
-
     numbers = [8, 3, 1, 2, 4]
     group = {2, 3, 5, 7}
-    found = sort_priority_fail(numbers, group)
+
+    found = False
+
+    def sort_key(elem: int) -> Tuple[int, int]:
+        if elem in group:
+            # `found` does not exist in the current scope; the assignment is treated as
+            # a variable definition, scoped within this function.
+            found = True
+            return (0, elem)
+        return (1, elem)
+
+    numbers.sort(key=sort_key)
 
     assert numbers == [2, 3, 1, 4, 8]
     assert not found
@@ -33,23 +29,19 @@ def test_sort_not_found() -> None:
 
 def test_sort_found() -> None:
     """Use `nonlocal` to traverse scope upon assignment for a variable."""
-
-    def sort_priority(numbers: List[int], group: Set[int]) -> bool:
-        found = False
-
-        def sort_key(elem: int) -> Tuple[int, int]:
-            nonlocal found
-            if elem in group:
-                found = True
-                return (0, elem)
-            return (1, elem)
-
-        numbers.sort(key=sort_key)
-        return found
-
     numbers = [8, 3, 1, 2, 4]
     group = {2, 3, 5, 7}
-    found = sort_priority(numbers, group)
+
+    found = False
+
+    def sort_key(elem: int) -> Tuple[int, int]:
+        nonlocal found
+        if elem in group:
+            found = True
+            return (0, elem)
+        return (1, elem)
+
+    numbers.sort(key=sort_key)
 
     assert numbers == [2, 3, 1, 4, 8]
     assert found
